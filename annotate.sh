5ec37f9626b1183fca580cd44f2df2c9d59125c7
Joel Uckelman <uckelman@nomic.net>
2015-07-03 12:27:37 +0100

    Converted annotate.sh to stop using pdftk.

--- annotate.sh
+++ annotate.sh
@@ -4,16 +4,17 @@ X=$1
 Y=$2
 TXT=$3
 SRC=$4
-P=$5
+PNUM=$5
+PAPER=${6:-a4paper}
+
 TMPDIR=$(mktemp -d)
-TEX=$TMPDIR/sig.tex
-STAMP=$TMPDIR/sig.pdf
+TEX=$TMPDIR/anno.tex
 PAGE=$TMPDIR/page.pdf
-SPAGE=$TMPDIR/spage.pdf
+SPAGE=$TMPDIR/anno.pdf
 DST=$(echo $SRC | sed -e 's/\.pdf$/-anno.pdf/')
 
 cat <<END >$TEX
-\\documentclass[a4paper]{article}
+\\documentclass[${PAPER}]{article}
 
 \\usepackage{fullpage}
 \\pagestyle{empty}
@@ -24,29 +25,29 @@ cat <<END >$TEX
 \\begin{document}
 
 \\begin{tikzpicture}[remember picture,overlay,shift=(current page.south west)]
+  \\node at (0,0) [anchor=south west] {\\includegraphics{${PAGE}}};
   \\node at (${X},${Y}) [anchor=base west] {$TXT};
 \\end{tikzpicture}
 
 \\end{document}
 END
 
-pdftk $SRC cat $P output $PAGE
+pdfseparate -f $PNUM -l $PNUM "$SRC" $PAGE
 pdflatex -output-directory $TMPDIR $TEX
 pdflatex -output-directory $TMPDIR $TEX
-pdftk $PAGE stamp $STAMP output $SPAGE
 rm -f $DST
 
-LASTPAGE=$(pdftk $SRC dump_data output | grep -Po '^NumberOfPages: \K\d+')
+LASTPAGE=$(pdfinfo "$SRC" | grep -Po '^Pages:\s+\K\d+')
 POST=
 PRE=
 
-if [ $P -ne 1 ]; then
-  PRE="A1-$(($P-1))"
+if [ $PNUM -ne 1 ]; then
+  PRE="$SRC 1-$((PNUM-1))"
 fi
 
-if [ $P -ne $LASTPAGE ]; then
-  POST="A$((P+1))-end"
+if [ $PNUM -ne $LASTPAGE ]; then
+  POST="$SRC $((PNUM+1))-"
 fi
 
-pdftk A=$SRC B=$SPAGE cat $PRE B1 $POST output $DST
+pdfjoin -o "$DST" $PRE $SPAGE 1 $POST
 rm -rf $TMPDIR
